56a493f86f084fb42e75d63644f3c6c173bc3570
David Mullins <Mullins6323@gmail.com>
2019-07-06 14:11:22 -0500

    authentication setup complete

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './Home.scss';
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h3>HOME</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
